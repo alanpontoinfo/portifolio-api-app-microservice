d6bc545e5a3a6286805f0600045b2624e43e2ae2
alansantos <alanpontoinfo@gmail.com>
2021-05-26 13:35:11 -0300

    add timestamp.html and change get to server

--- server.js
+++ server.js
@@ -29,14 +29,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp", function(req,res){
+app.get("/api", function(req,res){
   var now = new Date();
   res.json({"unix": now.getTime(),
             "utc": now.toUTCString()
 });
 });
 
-app.get("/api/timestamp/:date_string", function(req, res){
+app.get("/api/:date", function(req, res){
 
   let dateString = req.params.date_string;
 
@@ -50,7 +50,7 @@ if(parseInt(dateString)>10000){
   let passDateValue = new Date(dateString);
 
   if(passDateValue == "invalid Date"){
-  res.json({"error": "Invalid Date"});
+  res.json({error: "Invalid Date"});
 }
  else 
 { 
